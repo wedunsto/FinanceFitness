e1e984735fdd2aa4cbeb34aae5c21d76d85f3da0
William Dunston <wedunsto@aggies.ncat.edu>
2019-07-13 21:14:17 -0700

    Added code for counting time working out, and measuring funds earned working out and running

---
+++ app/src/main/java/com/example/financefitness/CalculateBudget.java
@@ -0,0 +1,38 @@
+package com.example.financefitness;
+
+import android.widget.TextView;
+import com.google.maps.android.SphericalUtil;
+import com.google.android.gms.maps.model.LatLng;
+
+public class CalculateBudget {
+    double fundsPerMinute;//Used to calculate money earned working out
+    double fundsPerMile;//Used to calculate money earned running
+    double distanceTraveled;//Used to hold distance travelled
+    double timeWorkedOut;//Used to hold workout time duration
+    double fundsGeneratedWorkingOut;//Used to hold total funds generated working out
+    double fundsGeneratedRunning;//Used to hold total funds generated running
+    double totalFundsGenerated;//Used to hold total funds generated working out and running
+
+    public CalculateBudget(){
+        fundsPerMinute = 0.84;
+        fundsPerMile = 10.0;
+        fundsGeneratedWorkingOut = 0;
+        fundsGeneratedRunning = 0;
+        totalFundsGenerated = 0;
+    }
+
+    public void calculateTimeWorkedOut(WorkOutTimer workOutTimer){//Used to display additional money earned working out
+        timeWorkedOut = workOutTimer.getTotalTimeWorkedOut();
+        fundsGeneratedRunning += (timeWorkedOut*fundsGeneratedWorkingOut);
+    }
+
+    public void calculateDistanceTraveled(LatLng start, LatLng stop){//Used to display additional money earned running
+        distanceTraveled = (SphericalUtil.computeDistanceBetween(start,stop)) * (0.000621371);//gets distance traveled walking in miles
+        fundsGeneratedRunning = distanceTraveled*fundsPerMile;
+    }
+
+    public void DisplayFundsGenerated(TextView textView){
+        totalFundsGenerated = fundsGeneratedWorkingOut + fundsGeneratedRunning;
+        textView.setText("$"+String.format("%.2f",totalFundsGenerated));
+    }
+}
---
+++ app/src/main/java/com/example/financefitness/WorkOutTimer.java
@@ -0,0 +1,39 @@
+package com.example.financefitness;
+
+import android.os.CountDownTimer;
+import android.widget.TextView;
+
+public class WorkOutTimer {
+    private CountDownTimer countdown;//incrementing timer
+    private int secondsPassed;//int variable to keep track of the seconds in a minute
+    private long totalTimeWorkedOut;//converts milliseconds to minutes
+
+    public void startWorkoutTimer(final TextView textView){//method to start the workout timer
+        secondsPassed = 0;
+        countdown = new CountDownTimer(3_600_000,1000){//displays time spent working out
+            @Override
+            public void onTick(long millisUntilFinished) {
+                totalTimeWorkedOut = (59 -(millisUntilFinished/6000));
+                secondsPassed +=1;
+                if(secondsPassed>59){//when seconds left equals 0 reset to 59
+                    secondsPassed=0;
+                }
+                textView.setText(totalTimeWorkedOut+" : "+secondsPassed);//sets the formatted time to text view
+            }
+
+            @Override
+            public void onFinish() {
+                textView.setText("You have worked out for an hour");//sets pre defined text when workout has reached an hour
+            }
+        }.start();
+    }
+
+    public void resetWorkoutTimer(){//Stops the timer and resets the timer
+        countdown.cancel();//stops the timer
+        totalTimeWorkedOut = 0;//resets the total time worked out variable
+    }
+
+    public Long getTotalTimeWorkedOut(){//returns the work out time
+        return totalTimeWorkedOut;
+    }
+}
